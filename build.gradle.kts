aecf3297bde7164774a7ae6a0019d3a9f81ca5cd
Jeremiah Zucker <jeremiah_zucker@intuit.com>
2021-05-10 11:32:43 -0700

    rename master to main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import AuthDelegate.Companion.auth
+import net.researchgate.release.GitAdapter.GitConfig
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.dokka.utilities.cast
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -35,13 +37,12 @@ apiValidation {
 }
 
 release {
-    failOnUpdateNeeded = false
     failOnPublishNeeded = false
     failOnSnapshotDependencies = false
 
-    preTagCommitMessage = "[skip ci] pre tag commit: "
-    tagCommitMessage = "[skip ci] creating tag: "
-    newVersionCommitMessage = "[skip ci] new version commit: "
+    getProperty("git").cast<GitConfig>().apply {
+        requireBranch = ""
+    }
 }
 
 nexusPublishing {
