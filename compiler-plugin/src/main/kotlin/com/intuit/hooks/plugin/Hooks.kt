96370d6ebebd0b588c16cee29d2fe42d20b7a794
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2021-10-06 16:53:21 -0400

    use fully qualified path for superclass

--- compiler-plugin/src/main/kotlin/com/intuit/hooks/plugin/Hooks.kt
+++ compiler-plugin/src/main/kotlin/com/intuit/hooks/plugin/Hooks.kt
@@ -25,7 +25,7 @@ internal val Meta.hooks: CliPlugin
                            |
                            |$imports
                            |
-                           |$kind ${name}Impl${this.`(typeParameters)`} : $name${this.`(typeParameters)`}() {
+                           |$kind ${name}Impl${this.`(typeParameters)`} : ${value.fqName}${this.`(typeParameters)`}() {
                            |   ${properties.map { it.property(null) }.joinToString("\n")}
                            |   ${classes.map { it.`class` }.joinToString("\n")} 
                            |}""".trimMargin().file("${name}Impl")
